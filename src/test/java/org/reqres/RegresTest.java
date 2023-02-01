11c14b0bb873387397166b0e6d757a2bf4447ed9
TrofimovaAM <TrofimovaAM@it.mos.ru>
2023-02-01 15:01:18 +0300

    update

--- src/test/java/org/reqres/RegresTest.java
+++ src/test/java/org/reqres/RegresTest.java
@@ -1,18 +1,13 @@
 package org.reqres;
 
 
-import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.reqres.api.*;
+import org.reqres.api.UserPayload;
 import org.reqres.api.services.UserApiService;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.not;
 
@@ -25,18 +20,6 @@ public class RegresTest {
     public void setUp() {
         RestAssured.baseURI = "https://reqres.in/";
     }
-
-//    @Test
-//    public void checkAvatarAndIdTest() {
-//        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
-//        List<UserData> users = given()
-//                .when()
-//                .get("org/reqres/api/users?page=2")
-//                .then().log().all()
-//                .extract().body().jsonPath().getList("data", UserData.class);
-//
-//        Assert.assertTrue(users.stream().allMatch(x->x.getEmail().endsWith("@reqres.in")));
-//    }
 
 
     @Test
